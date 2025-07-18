c58bcf31642b22df2514373caf15132073413e63
shend2025 <liugee@foxmail.com>
2025-07-18 18:47:40 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# Download configuration file
+
+# Download base URL, used for generating download links and QR codes
+DOWNLOAD_BASE_URL = "http://xxxxx"
+
+# Number of concurrent download threads
+DOWNLOAD_WORKERS = 5
+
+# File storage directory
+DESTINATION_DIR = "/xxxx"
+
+# Temporary file directory
+TEMP_DIR = "temp"
+
+# History record file
+HISTORY_FILE = "history.csv" 
